0c56307aa317b4896fd4322e64bad64a56b576e3
nmauersberg <n.mauersberg@gmail.com>
2022-03-14 23:55:33 +0100

    Authentication in context, automatic token refresh

---
+++ context/AuthContext.tsx
@@ -0,0 +1,40 @@
+import { User, getAuth } from 'firebase/auth';
+import nookies from 'nookies';
+import { createContext, useEffect, useState } from 'react';
+import firebase from '../firebase/clientApp';
+
+export const AuthContext = createContext<{ user: User | null }>({
+  user: null,
+});
+
+export function AuthProvider({ children }: any) {
+  const [user, setUser] = useState<User | null>(null);
+  const auth = getAuth(firebase);
+
+  useEffect(() => {
+    return auth.onIdTokenChanged(async (user) => {
+      if (!user) {
+        setUser(null);
+        nookies.set(undefined, 'token', '', { path: '/' });
+      } else {
+        const token = await user.getIdToken();
+        setUser(user);
+        nookies.set(undefined, 'token', token, { path: '/' });
+      }
+    });
+  }, [auth]);
+
+  useEffect(() => {
+    const handle = setInterval(async () => {
+      const user = auth.currentUser;
+      if (user) await user.getIdToken(true);
+    }, 10 * 60 * 1000);
+
+    // clean up setInterval
+    return () => clearInterval(handle);
+  }, [auth]);
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,21 +2,24 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Layout } from '../components/Layout';
+import { AuthProvider } from '../context/AuthContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <>
-        <Head>
-          <title>SOS Rassismus Barnim</title>
-          <meta
-            name='description'
-            content='Meta description for the Home page'
-          />
-        </Head>
-        <Component {...pageProps} />
-      </>
-    </Layout>
+    <AuthProvider>
+      <Layout>
+        <>
+          <Head>
+            <title>SOS Rassismus Barnim</title>
+            <meta
+              name='description'
+              content='Meta description for the Home page'
+            />
+          </Head>
+          <Component {...pageProps} />
+        </>
+      </Layout>
+    </AuthProvider>
   );
 }
 export default MyApp;
