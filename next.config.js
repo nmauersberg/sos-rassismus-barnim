b8efb5090a64d488e92033164d22b1344a24973c
nmauersberg <n.mauersberg@gmail.com>
2022-07-05 23:44:01 +0200

    Update dependencies and config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    prependData: `@import "./styles/vars.scss";`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
